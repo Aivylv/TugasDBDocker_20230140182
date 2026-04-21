642f55fc58de5d4ff552972848835c31a46c2567
aivylv <lyvialarasati@gmail.com>
2026-04-21 09:28:27 +0700

    Initial commit

---
+++ src/main/java/com/tugas/deployPertemuan7/DeployPertemuan7Application.java
@@ -0,0 +1,13 @@
+package com.tugas.deployPertemuan7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployPertemuan7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployPertemuan7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tugas/deployPertemuan7/DeployPertemuan7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tugas.deployPertemuan7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployPertemuan7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
